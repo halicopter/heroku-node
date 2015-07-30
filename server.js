e12502e3e3fdee80e536ca69ca9f6c0ce612903e
hali <hali@tinyrx.co>
2015-07-30 15:09:00 -0700

    trying new way of stormpath authentication

--- server.js
+++ server.js
@@ -4,33 +4,43 @@ function getEmailCount(stormpath, request, app) {
 		homedir = (process.platform === 'win32') ? process.env.HOMEPATH : process.env.HOME
 		keyfile = homedir + '/.stormpath/apiKey.properties';
 
-	stormpath.loadApiKey(keyfile, function apiKeyFileLoaded(err, apiKey) {
-    	if (err) throw err;
-    	client = new stormpath.Client({apiKey: apiKey});
-    	var app_url = 'https://api.stormpath.com/v1/applications/68pmLTnBHSvLhbbGIgn6Eb/';
+  app.use(stormpath.init(app, {
+    apiKeyId:     process.env.STORMPATH_API_KEY_ID,
+    apiKeySecret: process.env.STORMPATH_API_KEY_SECRET,
+    secretKey:    process.env.STORMPATH_SECRET_KEY,
+    application:  process.env.STORMPATH_URL,
+  }));
+  var apiKey = new stormpath.ApiKey(
+    process.env['STORMPATH_API_KEY_ID'],
+    process.env['STORMPATH_API_KEY_SECRET']
+  );
+  
+  var client = new stormpath.client({apikey: apikey});
 
-    	client.getApplication(app_url, function(err, sp_app) {
-     		if (err) throw err;
+  var app_url = 'https://api.stormpath.com/v1/applications/68pmLTnBHSvLhbbGIgn6Eb/';
 
-      		sp_app.getAccounts(function(err, accounts) {
-            app.set('port', (process.env.PORT || 5000));
-            app.get('/', function(req, res) {
-              size = accounts.size;
-              var payload = 
-                {
-                    "item": [
-                      { "text": "Emails", value: size},
-                      { "text": "", value: size}
-                    ]
-                };
-              res.send(JSON.stringify(payload));
-            });
-            app.listen(app.get('port'), function() {
-              console.log("running at localhost:" + app.get('port'))
-            });
-      	  });
-    	});  
-  	});
+  client.getApplication(app_url, function(err, sp_app) {
+     	if (err) throw err;
+
+      	sp_app.getAccounts(function(err, accounts) {
+          app.set('port', (process.env.PORT || 5000));
+          app.get('/', function(req, res) {
+            size = accounts.size;
+            var payload = 
+              {
+                  "item": [
+                    { "text": "Emails", value: size},
+                    { "text": "", value: size}
+                  ]
+              };
+            res.send(JSON.stringify(payload));
+          });
+          app.listen(app.get('port'), function() {
+            console.log("running at localhost:" + app.get('port'))
+          });
+      	});
+  });  
+
 }
 
 //need to add stuff to file so this can be run/tested on heroku
@@ -38,7 +48,7 @@ function getEmailCount(stormpath, request, app) {
 
 
 (function(){
-	var stormpath = require('stormpath')
+	var stormpath = require('express-stormpath')
 	    app = require('express')()
 	    request = require('request');
 	getEmailCount(stormpath, request, app);
